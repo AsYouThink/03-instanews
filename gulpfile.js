287d594a5c9e78018ea3de0745808e4712b16bb0
DildarRana <dildar.rana@venora.no>
2019-05-14 19:26:20 -0700

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-// Require Gulp first!
-const gulp = require("gulp");
-// This is a very basic Gulp task,
-// with a name and some code to run
-// when this task is called:
-gulp.task("default", function(done) {
-  console.log("Hello world");
-  done();
+const gulp = require("gulp"); // Load Gulp!
+// Now that we've installed the terser package we can require it:
+const terser = require("gulp-terser"),
+  rename = require("gulp-rename");
+gulp.task("default", function() {
+  return gulp
+    .src("./js/*.js") // What files do we want gulp to consume?
+    .pipe(terser()) // Call the terser function on these files
+    .pipe(rename({ extname: ".min.js" })) // Rename the uglified file
+    .pipe(gulp.dest("./build/js")); // Where do we put the result?
 });
